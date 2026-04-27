a12d83947669c7b8fe821e1853b39e9e57b14d29
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2026-04-27 16:52:37 -0700

    Update StateBasedMachine.java
    Did some work on writing the state based machine, but it is far from finished.

--- StateBasedMachine.java
+++ StateBasedMachine.java
@@ -1,1 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import java.util.Collections;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import javafx.util.pair;
+
+public class StateBasedMachine {
+	// Declare OpMode members.
+private list<
+	public void init() {
+	}
+  
+	// parameters are all doubles
+	// data: from auto/gamepad, to drive motors
+	public void SBM() {
+
+	}
+}
 
