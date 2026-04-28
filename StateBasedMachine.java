de654c5eeaa55d96d4562b11568fe2a9e7c01041
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2026-04-28 16:57:18 -0700

    Update StateBasedMachine.java
    Added multiple lists for things to keep track of, including object, time, reset type, and reset state.

--- StateBasedMachine.java
+++ StateBasedMachine.java
@@ -1,22 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import java.util.ArrayList;
 import java.util.Collections;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import javafx.util.pair;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class StateBasedMachine {
-	// Declare OpMode members.
-private List<Map.Entry<String, Integer>> list = new ArrayList<>();
-list.add(new AbstractMap.SimpleEntry<>("motor1", 50));
-	public void init() {
-	}
-  
-	// parameters are all doubles
-	// data: from auto/gamepad, to drive motors
-	public void SBM() {
+	//Operation list are the objects currently active
+	private String[] object;
+	//Time list is the time in miliseconds when the corresponding object needs to be turned off
+	private String[] time;
+	private String[] resetType;
+	private int[] resetState;
 
+	public void AddSBM(String object, int length, int runtime, String Type, int State) {
+		
+	}
+	
+	public void checkSBM(int runtime) {
+		for (int i = 0; i=operations.length; i++) {
+			if (time[i]>runtime) {
+				if (resetType[i]="Motor") {
+					object[i].setPower(resetState[i]);
+				}
+			}
+		}
 	}
 }
-
